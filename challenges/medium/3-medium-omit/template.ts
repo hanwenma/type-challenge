094be6c6c9ec20265dd4a3f06deeccafe5d3d054
mhw <1768263460@qq.com>
2022-03-04 15:22:32 +0800

    feat: finish 3-medium-omit

---
+++ challenges/medium/3-medium-omit/template.ts
@@ -0,0 +1,13 @@
+/**
+ * 关键点：
+*/
+
+// 方式一
+// type MyOmit<T, K> = {
+//   [Key in keyof T as Key extends K ? never : Key]: T[Key]
+// }
+
+// 方式二
+type MyOmit<T, K> = {
+  [Key in MyExclude<keyof T, K>]: T[Key]
+}
