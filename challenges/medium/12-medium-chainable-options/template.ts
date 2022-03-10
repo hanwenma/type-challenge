ce0b430c1f47b462266f1ad6ce0beea1795c0a55
mhw <1768263460@qq.com>
2022-03-10 22:22:47 +0800

    feat: finish 12-medium-chainable-options

---
+++ challenges/medium/12-medium-chainable-options/template.ts
@@ -0,0 +1,8 @@
+/**
+ * 关键点：
+*/
+
+type Chainable<T extends {[key:string]: unknown} = {}> = {
+  option<K extends string, V>(key: Exclude<K, keyof T>, value: V): Chainable<T & {[k in K]: V}>
+  get(): T
+}
