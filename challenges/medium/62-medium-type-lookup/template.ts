7b198a104b452a3fd7dd3dca71e46e9833c97035
mhw <1768263460@qq.com>
2022-03-17 18:36:12 +0800

    feat: finish 62-medium-type-lookup

---
+++ challenges/medium/62-medium-type-lookup/template.ts
@@ -0,0 +1,9 @@
+/**
+ * 关键点：
+*/
+
+// 方式一
+type LookUp<U, T> = U extends { type: T } ? U : never
+
+// 方式二
+// type LookUp<U, T extends string> = U extends { type: infer V } ? T extends V ? U : never : never
