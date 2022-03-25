a4e982b6bf33f438bea9b3ffc3e77b03c1217483
mhw <1768263460@qq.com>
2022-03-25 21:42:58 +0800

    feat: finish 116-medium-replace

---
+++ challenges/medium/116-medium-replace/template.ts
@@ -0,0 +1,12 @@
+/**
+ * 关键点：
+*/
+
+type Replace<S extends string, From extends string, To extends string> =
+  From extends ''
+  ? S
+  : S extends `${infer V1}${From}${infer V2}`
+    ? `${V1}${To}${V2}`
+    : S
+
+type A = Replace<'foobarbar', '', 'foo'>
