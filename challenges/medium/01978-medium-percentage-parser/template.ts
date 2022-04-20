04b0f845f0ed3218a4a2d34d9465e162c29a6888
mhw <1768263460@qq.com>
2022-04-20 22:09:09 +0800

    feat: finish 01978-medium-percentage-parser

---
+++ challenges/medium/01978-medium-percentage-parser/template.ts
@@ -0,0 +1,16 @@
+
+type MatchSymbol<T> =
+  T extends `${infer F}${infer _}`
+  ? F extends '+' | '-'
+  ? F
+  : ''
+  : '';
+
+type MatchUnit<T> = T extends `${infer _}%` ? '%' : '';
+
+type MatchValue<T> =
+  T extends `${MatchSymbol<T>}${infer V}${MatchUnit<T>}`
+  ? V
+  : '';
+
+type PercentageParser<A extends string> = [MatchSymbol<A>, MatchValue<A>, MatchUnit<A>];
